443cbac36bab84fc7b3d333c64241e4023325419
Jeferson Silva <jeferson.i.silva@gmail.com>
2023-11-25 00:08:28 -0300

    build

--- docs/nuxt-assets/account.80ae4e30.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./nuxt-layout.2c484a52.js";import{n as o,w as a,o as _,d as n}from"./entry.befd1641.js";const i={__name:"account",props:{modelValue:{type:[String],default:""}},emits:["update:modelValue"],setup(m,{emit:p}){return(c,r)=>{const t=e;return _(),o(t,{name:"app"},{main:a(()=>[n(" xxx ")]),_:1})}}};export{i as default};
